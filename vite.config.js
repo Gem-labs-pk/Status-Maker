61bdeeaa251d54b8f5bbc0a363e9231b0f5a84a4
Muhammad Saqib Zahid <m.saqibzahid.iqbal@gmail.com>
2026-02-11 19:19:40 +0500

    Setting file path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Status-Maker/", // This is the critical fix
+  base: "./", // <--- USE THIS (Dot Slash). It works for ANY repo name.
 })
